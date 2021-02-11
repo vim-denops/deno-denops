077f1ce52413ee7b6a75cda9a6b5025f74001111
Alisue <lambdalisue@hashnote.net>
2021-02-12 02:53:09 +0900

    Add mod.ts

---
+++ mod.ts
@@ -0,0 +1,7 @@
+export * from "./context.ts";
+export * from "./denops.ts";
+export * from "./facade.ts";
+export * from "./facade/vim.ts";
+export * from "./facade/neovim.ts";
+export * from "./plugin.ts";
+export * from "./server.ts";
